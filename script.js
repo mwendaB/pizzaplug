1300e82e7317715775bd5d6b184e7b7ce3cf420b
mwendaB <brian.mwenda@moringaschool.com>
2021-09-19 17:21:15 +0300

    add js file and make few changes to the html file

--- script.js
+++ script.js
@@ -46,3 +46,22 @@ function tikkaSaucee() {
   }
   return parseInt(tikkaSauce);
 }
+
+
+function mozarellaCheese() {
+  var cheese = 0;
+  var addmozarellaCheese = document.getElementById("toppingone");
+  if (addmozarellaCheese.checked === true) {
+      cheese = 110;
+  }
+  return parseInt(cheese);
+}
+
+function calctotalPrice(e) {
+  event.preventDefault();
+  var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
+  
+      console.log(totalPrice);
+  alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." )
+
+}
