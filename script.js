a78c97d26443bebb933efe0df8f60738f6813afc
mwendaB <brian.mwenda@moringaschool.com>
2021-09-19 16:46:22 +0300

    add js

--- script.js
+++ script.js
@@ -28,37 +28,4 @@ function sausages() {
   }
   return parseInt(sausage);
 }
-function onions() {
-  var onion = 0;
-  var addonions = document.getElementById("toppingone");
-  if (addMushroom.checked === true) {
-      onion = 130;
-  }
-  return parseInt(onion);
-}
-function tikkaSaucee() {
-  var tikkaSauce = 0;
-  var addtikkaSauce = document.getElementById("toppingone");
-  if (addtikkaSauce.checked === true) {
-      tikkaSauce = 120;
-  }
-  return parseInt(tikkaSauce);
-}
-function mozarellaCheese() {
-  var cheese = 0;
-  var addmozarellaCheese = document.getElementById("toppingone");
-  if (addmozarellaCheese.checked === true) {
-      cheese = 110;
-  }
-  return parseInt(cheese);
-}
-
-function calctotalPrice(e) {
-  event.preventDefault();
-  var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
-  
-      console.log(totalPrice);
-  alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." )
-
-}
 
