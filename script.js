39c9c1fd7b9fde7faf946dd8f7a5cf404e3870c8
mwendaB <brian.mwenda@moringaschool.com>
2021-09-20 09:35:28 +0300

    added the price section

--- script.js
+++ script.js
@@ -7,3 +7,65 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+
+
+
+$(document).ready(function(){
+  $("button.order").click(function(event){
+   let pname = $(".name option:selected").val();
+   let psize = $("#size option:selected").val();
+   let pcrust = $("#crust option:selected").val();
+   let ptopping = [];
+   $.each($("input[name='toppings']:checked"), function(){            
+       ptopping.push($(this).val());
+   });
+   console.log(ptopping.join(", "));
+   switch(psize){
+    case "0":
+      price =0;
+    break;
+    case "large":
+       price = 1500;
+       console.log(price);
+     break;
+     case "medium":
+       price = 1000;
+       console.log("The price is "+price);
+     break;
+     case "small":
+       price = 800;
+       console.log(price);
+     default:
+       console.log("error"); 
+   }
+   switch(pcrust){
+      case "0":
+        crust_price = 0;
+      break;
+      case "Crispy":
+        crust_price = 200;
+      break;
+      case "Stuffed":
+        crust_price = 250;
+      break;
+      case "Gluten-free":
+        crust_price = 180;
+      break;
+      default:
+        console.log("No price"); 
+    }
+    let topping_value = ptopping.length*150;
+    console.log("toppins value" + topping_value);
+
+    if((psize == "0") && (pcrust == "0")){
+      console.log("nothing selected");
+      $("button.order").show();
+      $("#information").show();
+      $("div.choise").hide();
+      alert("Please select pizza size and crust"); 
+    }
+    else{
+      $("button.order").hide();
+      $("#information").hide();
+      $("div.choise").slideDown(1000);
+    }
